7aa9d33fd25ae9691de752ff0cb1447fbd100dd5
Sara-y2 <79771350+Sara-y2@users.noreply.github.com>
2022-01-04 17:11:44 +0100

    Update ManagerArea.js

--- client/src/Components/ManagerArea.js
+++ client/src/Components/ManagerArea.js
@@ -151,8 +151,32 @@ function ManagerArea(props) {
             </div>
           </div>
         </div>
+     <div className="card m-3 d-block shadow">
+          <div className="row no-gutters">
+            <div className="col-md-4 p-3 text-center" style={{ backgroundColor: "#f5f5dc" }}>
+              {walletIcon}
+            </div>
+            <div className="col-md-8">
+              <div className="card-body">
+                <h5 className="card-title">List of past pickups </h5>
+                <p className="card-text">
+                  •Mark as missed the past pickups<br />
+                  
+                </p>
+                <div className="d-block text-end">
+                  <Link to="/see-pickups">
+                    <button className="btn" style={{ backgroundColor: "#f5f5dc", color: "black" }}>See missed pickups </button>
+                  </Link>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
+
+
+    
   );
 }
 
@@ -163,4 +187,16 @@ const pendingApps =
 const acceptedApps =
   <svg width="128" height="128" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="users" className="svg-inline--fa fa-users" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512"><path fill="currentColor" d="M319.9 320c57.41 0 103.1-46.56 103.1-104c0-57.44-46.54-104-103.1-104c-57.41 0-103.1 46.56-103.1 104C215.9 273.4 262.5 320 319.9 320zM369.9 352H270.1C191.6 352 128 411.7 128 485.3C128 500.1 140.7 512 156.4 512h327.2C499.3 512 512 500.1 512 485.3C512 411.7 448.4 352 369.9 352zM512 160c44.18 0 80-35.82 80-80S556.2 0 512 0c-44.18 0-80 35.82-80 80S467.8 160 512 160zM183.9 216c0-5.449 .9824-10.63 1.609-15.91C174.6 194.1 162.6 192 149.9 192H88.08C39.44 192 0 233.8 0 285.3C0 295.6 7.887 304 17.62 304h199.5C196.7 280.2 183.9 249.7 183.9 216zM128 160c44.18 0 80-35.82 80-80S172.2 0 128 0C83.82 0 48 35.82 48 80S83.82 160 128 160zM551.9 192h-61.84c-12.8 0-24.88 3.037-35.86 8.24C454.8 205.5 455.8 210.6 455.8 216c0 33.71-12.78 64.21-33.16 88h199.7C632.1 304 640 295.6 640 285.3C640 233.8 600.6 192 551.9 192z"></path></svg>
 
+const walletIcon = (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="128"
+    height="128"
+    fill="currentColor"
+    className="bi bi-wallet"
+    viewBox="0 0 16 16"
+  >
+    <path d="M0 3a2 2 0 0 1 2-2h13.5a.5.5 0 0 1 0 1H15v2a1 1 0 0 1 1 1v8.5a1.5 1.5 0 0 1-1.5 1.5h-12A2.5 2.5 0 0 1 0 12.5V3zm1 1.732V12.5A1.5 1.5 0 0 0 2.5 14h12a.5.5 0 0 0 .5-.5V5H2a1.99 1.99 0 0 1-1-.268zM1 3a1 1 0 0 0 1 1h12V2H2a1 1 0 0 0-1 1z" />
+  </svg>
+);
 export default ManagerArea;
