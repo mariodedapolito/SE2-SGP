9740c3d88b4f2fc18be62bc05a71fc06ee38feaf
Sara-y2 <79771350+Sara-y2@users.noreply.github.com>
2022-01-08 15:26:48 +0100

    Update FarmerPageOrders.test.js

--- client/src/__tests__/FarmerPageOrders.test.js
+++ client/src/__tests__/FarmerPageOrders.test.js
@@ -6,7 +6,7 @@ import dayjs from 'dayjs';
 const testOrders = [{ order_id: 1, client_id: 2, product_name: "carrots",product_id:1,order_quantity:1,state:"booked",farmer_state:null,OrderPrice: 1.23, id:1,address:"via Legnano",city:"torino",zipcode:10155, Nation:"Italy",date:"2021-12-21",time:"11:00" }];
 const time=[{ date: "2021-12-01" , hour: "11:00" }];
 const testClients = [{ client_id: 2, budget:0, name: "Alan", surname: "Green", gender: 1, birthday: "1995-01-15", country: "Italy",region: "Piemonte",address: "Via Gardena 19",city: "Torino"}];
-const products =[ { product_id: 1, product_name: 'carrots', product_description: 'Some description 1',category: 'Vegetables',  price: 1.23, unit: 'kg',quantity: 110, expiryDate: '2021-11-20', providerId: 1, providerName: 'Luca Bianchi', active: 1, qty: 1,}];
+const products =[ { id: 1, name: 'carrots', description: 'Some description 1',category: 'Vegetables',  price: 1.23, unit: 'kg',quantity: 110, expiryDate: '2021-11-20', providerId: 1, providerName: 'Luca Bianchi', active: 1, qty: 1,}];
  
 test('Renders Correctly the bookings for a  farmer', () => { 
 const renderResult = render(<Fbookings orders={testOrders} time={time}products={products} providerid={1}clients={testClients}/>);
